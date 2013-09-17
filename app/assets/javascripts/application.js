069d6b533c5a3e0649d5606ca6149b40ad10ec5b
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-09-17 17:57:19 +0900

    Setup ember.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ DemoApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.ember.variant = :production
 end
