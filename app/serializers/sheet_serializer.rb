a32a8c3e783408ce287dc72a57889653537cf5a9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-09-23 11:47:44 +0900

    Return embedded objects from sheet API

--- app/serializers/sheet_serializer.rb
+++ app/serializers/sheet_serializer.rb
@@ -1,5 +1,5 @@
 class SheetSerializer < ActiveModel::Serializer
   attributes :id, :created_at
 
-  has_many :order_lines, embed: :ids, include: true
+  has_many :order_lines, embed: :objects, include: true
 end
